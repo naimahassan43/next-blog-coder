8642955976f356f735cb03cebb504f44208032ed
Naima Hassan <naima.hassan434@gmail.com>
2022-07-04 17:07:17 +0100

    Blog and Blogpost page

--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,42 @@
-import React from "react";
+import Link from "next/link";
+import styles from "../styles/Blog.module.css";
 
 const BlogPage = () => {
-  return <div>blog</div>;
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h2>Popular Blogs</h2>
+        <div className={styles.blogItem}>
+          <Link href={"/blogpost/learn-js"}>
+            <h3>How to learn JavaScript in 2022?</h3>
+          </Link>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit
+            temporibus magnam at architecto dolorum nisi eaque nihil quod
+            perferendis sapiente.
+          </p>
+        </div>
+        <div className="blogItem">
+          <h3>How to learn React Js in 2022?</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit
+            temporibus magnam at architecto dolorum nisi eaque nihil quod
+            perferendis sapiente.
+          </p>
+        </div>
+
+        <div className="blogItem">
+          <h3>How to learn React Js in 2022?</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit
+            temporibus magnam at architecto dolorum nisi eaque nihil quod
+            perferendis sapiente.
+          </p>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default BlogPage;
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,9 +1,24 @@
 import { useRouter } from "next/router";
+import styles from "../../styles/BlogPost.module.css";
 
 const slug = () => {
   const router = useRouter();
   const { slug } = router.query;
-  return <div>{slug}</div>;
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>Title of the page {slug}</h1>
+        <hr />
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum at,
+          aspernatur ad voluptate exercitationem totam consequuntur velit? Vero,
+          recusandae. Quis ducimus natus fugiat veniam aliquid quae hic
+          voluptates corporis? Explicabo esse suscipit perspiciatis dolores
+          necessitatibus placeat possimus magni ducimus minus!
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default slug;
